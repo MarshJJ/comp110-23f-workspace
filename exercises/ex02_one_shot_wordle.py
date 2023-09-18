8c06610691657735d0483436c2a136786f7e61d2
Marshall <marshjj@ad.unc.edu>
2023-09-18 19:14:42 -0400

    ex_02 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -8,7 +8,7 @@ user_guess: str
 
 real_guess: int = 0
 
-while(real_guess < 1):
+while (real_guess < 1):
     user_guess = input("What is your 6-letter guess? ")
     if (len(user_guess) != len(secret_word)):
         print("That was not 6 letters! Try again: ")
@@ -29,14 +29,14 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-while(i < len(secret_word)):
+while (i < len(secret_word)):
     j = 0
     yellow = 0
-    if(user_guess[i] == secret_word[i]):
+    if (user_guess[i] == secret_word[i]):
         emoji_boxes += GREEN_BOX
         i += 1
     else:
-        while(j < len(secret_word)):
+        while (j < len(secret_word)):
             if(user_guess[i] == secret_word[j]):
                 emoji_boxes += YELLOW_BOX
                 j += len(secret_word)
@@ -44,7 +44,7 @@ while(i < len(secret_word)):
                 yellow += 1
             else:
                 j += 1
-        if(yellow != 1):
+        if (yellow != 1):
             emoji_boxes += WHITE_BOX
             i += 1
 print(emoji_boxes)
